ac603a43e1e5ef495e5594b31f5a063b3c885cd7
Anderson Pires <148875704+andershow31@users.noreply.github.com>
2025-03-26 21:35:43 -0300

    findById implements

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,19 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
 		
-		Seller seller = new Seller(21, "Anderson", "anderson@gmail.com", new Date(), 3000.,obj);
-		
-		SellerDao sellerdao = DaoFactory.createSellerDao();
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		//assim o programa principal não vai conhecer a implementação, mas somente a interface
 
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
-	
 	}
 
 }
